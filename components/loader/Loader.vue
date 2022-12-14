3cc77fe97765e8a47db7b0dae6a683851115cfd0
zzeordyga <47037524+zzeordyga@users.noreply.github.com>
2022-12-14 08:28:36 +0700

    feat: sixth component without draggable because gsap plugin is not wokring

---
+++ components/loader/Loader.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="loader">
+
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.loader {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #fff;
+    z-index: 1050;
+    display: none;
+    transition: all .5s ease;
+    opacity: 1;
+}
+</style>
--- components/template/ComponentSix.vue
+++ components/template/ComponentSix.vue
@@ -4,16 +4,20 @@
             <div class="columns right">
                 <div class="column flickity-enabled is-draggable">
                     <div class="flickity-viewport">
-                        <div class="flickity-slider">
+                        <div class="flickity-slider"
+                            :style="{ left: '0px', transform: `translate(${currSlide * -100}%)` }"
+                            ref="sliderContainer">
                             <div v-for="(item, i) in data" :key="i"
-                                :class="`slide ${currSlide === i ?? 'is-selected'}`">
+                                :class="`slide ${currSlide === i ? 'is-selected' : ''}`"
+                                :style="{ left: `${i * 100}%` }" :aria-hidden="currSlide !== i">
                                 <p v-html="item.description"></p>
                                 <small v-html="item.title"></small>
                             </div>
                         </div>
                     </div>
                     <ol class="flickity-page-dots">
-                        <li v-for="(_, i) in data" :key="i" class="dot" :aria-label="`Page dot ${i}`"></li>
+                        <li v-for="(_, i) in data" :key="i" :class="`dot ${currSlide === i ? 'is-selected' : ''}`"
+                            :aria-label="`Page dot ${i}`" @click="currSlide = i; switchInterval()"></li>
                     </ol>
                 </div>
             </div>
@@ -22,9 +26,57 @@
 </template>
 
 <script setup lang="ts">
+// import { gsap } from 'gsap'
+// import Draggable from 'gsap/Draggable'
+// gsap.registerPlugin(Draggable)
+
 const pr = defineProps(['model'])
 const data = pr.model?.dataSlider
 const currSlide = ref(0)
+const sliderContainer = ref()
+
+const delay = 5000
+const intervalId = ref()
+
+const followMouse = (e: any) => {
+
+}
+
+const switchInterval = () => {
+    if (intervalId.value !== null) clearInterval(intervalId.value);
+    intervalId.value = setInterval(() => {
+        next()
+    }, delay)
+}
+
+const next = () => {
+    if (currSlide.value < data?.length - 1)
+        currSlide.value++;
+    else currSlide.value = 0
+
+    switchInterval()
+}
+
+onMounted(() => {
+    intervalId.value = setInterval(() => {
+        next()
+    }, delay)
+
+    // Draggable.create('.slide', {
+    //     type: 'x',
+    //     bounds: sliderContainer.value,
+    //     onClick: e => {
+    //         console.log(e)
+    //     },
+    //     onDragEnd: e => {
+    //         console.log(`Direction From! ${e}`)
+    //     }
+    // })
+})
+
+onUnmounted(() => {
+    () => clearInterval(intervalId.value)
+})
 </script>
 
 <style scoped lang="scss">
@@ -65,6 +117,7 @@ const currSlide = ref(0)
     & .slide {
         width: 100%;
         padding-bottom: 50px;
+        position: absolute;
     }
 
     & p {
